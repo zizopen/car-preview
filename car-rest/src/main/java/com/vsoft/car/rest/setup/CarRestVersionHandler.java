5e364f4322b4dbfde02ab483fcbdae1f012d46df
Hung Vu Manh <hung.vu02@pyramid-consulting.com>
2015-06-01 23:44:58 +0700

    Initial project

---
+++ car-rest/src/main/java/com/vsoft/car/rest/setup/CarRestVersionHandler.java
@@ -0,0 +1,14 @@
+package com.vsoft.car.rest.setup;
+
+import info.magnolia.module.DefaultModuleVersionHandler;
+
+/**
+ * This class is optional and lets you manager the versions of your module,
+ * by registering "deltas" to maintain the module's configuration, or other type of content.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ *
+ * @author hung.vu02
+ */
+public class CarRestVersionHandler extends DefaultModuleVersionHandler {
+
+}
